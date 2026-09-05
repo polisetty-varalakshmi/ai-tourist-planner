dec44705dbc88bf79aa4685a883dcc306ac8d279
Varalakshmi <varalakshmipolisetty531@gmail.com>
2026-09-05 15:48:38 +0530

    Fix frontend CORS

--- server/server.js
+++ server/server.js
@@ -17,7 +17,11 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ai-tourist-planner-opal.vercel.app",
+  })
+);
 app.use(express.json());
 
 // Check API key
